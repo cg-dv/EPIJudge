0e867f6e40aedb589496fb6416c002e9c7322e7f
cg-dv <24211251+cg-dv@users.noreply.github.com>
2023-07-28 08:32:50 -0700

    Added h_index.py solution

--- epi_judge_python/h_index.py
+++ epi_judge_python/h_index.py
@@ -4,8 +4,13 @@ from test_framework import generic_test
 
 
 def h_index(citations: List[int]) -> int:
-    # TODO - you fill in here.
-    return 0
+    h_index = 0
+    citations = reversed(sorted(citations))
+    
+    for i, citation_count in enumerate(citations):
+        if (citation_count >= (i + 1)) and citation_count >= 1:
+            h_index += 1
+    return h_index 
 
 
 if __name__ == '__main__':
